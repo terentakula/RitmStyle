c21f6abd15fa12f14baad1cd0ae081ff1f5e0813
Alexander Terentiev <terentiev.grizzly@gmail.com>
2026-09-12 17:40:50 +0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './styles.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./styles.css";
 
-const basename = import.meta.env.BASE_URL.replace(/\/$/, '') || '/'
+const basename = import.meta.env.BASE_URL.replace(/\/$/, "") || "/";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </StrictMode>,
-)
+);
